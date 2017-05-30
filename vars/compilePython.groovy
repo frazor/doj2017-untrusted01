dfc9e98d7ce3c41ed08e5acb72eb3107a15e9345
Fredrik Luthander <fredrik.luthander@esss.se>
2017-05-30 16:29:10 +0200

    add compile parameter

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,10 +1,4 @@
-def call() {
-	sh(libraryResource('eu/inuits/python.sh'))
-//	sh """find . -name '*.py' -print0 |
-//		xargs -0 -t -L 1 python2 -m py_compile"""
-}
-
-def call(){
+def call(String directory='.'){
 	if (fileExists("requirements.txt")){
 		sh "virtualenv venv"
 		sh "venv/bin/pip install -r requirements.txt"
