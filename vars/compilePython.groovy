e897a5c094715349d984653ed21b539319ec3eb4
Fredrik Luthander <fredrik.luthander@esss.se>
2017-05-30 16:23:47 +0200

    compilePython step fixed

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,5 +1,5 @@
 def call() {
 	sh(libraryResource('eu/inuits/python.sh'))
+//	sh """find . -name '*.py' -print0 |
+//		xargs -0 -t -L 1 python2 -m py_compile"""
 }
-#	sh """find . -name '*.py' -print0 |
-#		xargs -0 -t -L 1 python2 -m py_compile"""
